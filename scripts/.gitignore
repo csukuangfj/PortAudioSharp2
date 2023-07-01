7615f0901fc2f95744085d42ef21f84d83dedaa7
Fangjun Kuang <csukuangfj@gmail.com>
2023-07-01 18:26:54 +0800

    build nuget package

---
+++ scripts/.gitignore
@@ -0,0 +1,3 @@
+linux
+windows
+macos
---
+++ scripts/run.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Copyright (c)  2023  Xiaomi Corporation
+
+set -ex
+
+mkdir -p macos linux windows all
+
+cp -v ./libportaudio.dylib ./macos
+cp -v ./libportaudio.so ./linux
+cp -v ./portaudio.dll ./windows
+
+./generate.py
+
+pushd linux
+dotnet build -c Release
+dotnet pack -c Release -o ../../PortAudioSharp/packages
+popd
+
+pushd macos
+dotnet build -c Release
+dotnet pack -c Release -o ../../PortAudioSharp/packages
+popd
+
+pushd windows
+dotnet build -c Release
+dotnet pack -c Release -o ../../PortAudioSharp/packages
+popd
+
+pushd ../PortAudioSharp
+dotnet build -c Release
+dotnet pack -c Release -o ./packages
+popd
+
+mv ../PortAudioSharp/packages .
