0db168fe061d6f13becaab8a808cee62d5d5c70d
Fangjun Kuang <csukuangfj@gmail.com>
2024-06-27 20:03:42 +0800

    Release v1.0.3 (#26)

--- PortAudioSharp/Structures/DeviceInfo.cs
+++ PortAudioSharp/Structures/DeviceInfo.cs
@@ -10,6 +10,47 @@ using Time = System.Double;
 
 namespace PortAudioSharp
 {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DeviceInfoData
+    {
+        public int structVersion;           // this is struct version 2
+
+        public IntPtr name;                 // Originally: `const char *`
+
+        public HostApiIndex hostApi;        // note this is a host API index, not a type id
+
+        public int maxInputChannels;
+        public int maxOutputChannels;
+
+        // Default latency values for interactive performance.
+        public Time defaultLowInputLatency;
+        public Time defaultLowOutputLatency;
+
+        // Default latency values for robust non-interactive applications (eg. playing sound files).
+        public Time defaultHighInputLatency;
+        public Time defaultHighOutputLatency;
+
+        public double defaultSampleRate;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("DeviceInfo [");
+            sb.AppendLine($"  structVersion={structVersion}");
+            sb.AppendLine($"  name={name}");
+            sb.AppendLine($"  hostApi={hostApi}");
+            sb.AppendLine($"  maxInputChannels={maxInputChannels}");
+            sb.AppendLine($"  maxOutputChannels={maxOutputChannels}");
+            sb.AppendLine($"  defaultSampleRate={defaultSampleRate}");
+            sb.AppendLine($"  defaultLowInputLatency={defaultLowInputLatency}");
+            sb.AppendLine($"  defaultLowOutputLatency={defaultLowOutputLatency}");
+            sb.AppendLine($"  defaultHighInputLatency={defaultHighInputLatency}");
+            sb.AppendLine($"  defaultHighOutputLatency={defaultHighOutputLatency}");
+            sb.AppendLine($"  defaultHighSampleRate={defaultSampleRate}");
+            sb.AppendLine("]");
+            return sb.ToString();
+        }
+    }
     /// <summary>
     /// A structure providing information and capabilities of PortAudio devices.
     /// Devices may support input, output or both input and output.
